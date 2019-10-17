8ed15b5b4d95846d90c06fc1fdf339225adfcb0f
szilard-molnar <mszilard86@gmail.com>
2019-10-17 18:31:12 -0400

    scraping from index.hu

--- server.js
+++ server.js
@@ -50,12 +50,12 @@ app.get("/", (req,res) => {
     // A GET route for scraping the echoJS website
     app.get("/scrape", function (req, res) {
         // First, we grab the body of the html with axios
-        axios.get("http://www.echojs.com/").then(function (response) {
+        axios.get("http://www.index.hu/").then(function (response) {
             // Then, we load that into cheerio and save it to $ for a shorthand selector
             var $ = cheerio.load(response.data);
 
             // Now, we grab every h2 within an article tag, and do the following:
-            $("article h2").each(function (i, element) {
+            $("div h1").each(function (i, element) {
                 // Save an empty result object
                 var result = {};
 
